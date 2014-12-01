afa03c55057ecd001a3cffcc93b03d00554aaefa
Naoki Kaneshiro <kaneshiro@rightcode.co.jp>
2014-12-01 14:03:04 +0900

    Add Dimentions and Reviews.

--- amzpa_test.go
+++ amzpa_test.go
@@ -8,9 +8,12 @@
 package goamzpa
 
 import (
+	"encoding/xml"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -74,6 +77,113 @@ func Test_ItemSearch_Books(t *testing.T) {
 	checkAndWriteFile(t, file, response, err)
 }
 
+func Test_Unmarshal_ItemLookup(t *testing.T) {
+	content, err := ioutil.ReadFile("./testfiles/Test_ItemLookup.xml")
+	if content == nil || err != nil {
+		t.Error(err)
+	}
+
+	itemLookupResponse := ItemLookupResponse{}
+	err = xml.Unmarshal(content, &itemLookupResponse)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !itemLookupResponse.Request.IsValid {
+		t.Error("IsValid:", itemLookupResponse.Request.IsValid)
+	}
+
+	for i, item := range itemLookupResponse.Items {
+		fmt.Println("-------------------")
+		fmt.Printf("Item %d\n", i)
+		fmt.Println("-------------------")
+		fmt.Printf("ASIN: %s\n", item.ASIN)
+		fmt.Printf("DetailPageURL: %s\n", item.DetailPageURL)
+		fmt.Println()
+
+		fmt.Println("  -------------------")
+		fmt.Println("  ItemLinks")
+		fmt.Println("  -------------------")
+		for j, itemLink := range item.ItemLinks {
+			fmt.Printf("  ItemLink %d\n", j)
+			fmt.Printf("  Description: %s\n", itemLink.Description)
+			fmt.Printf("  URL: %s\n", itemLink.URL)
+			fmt.Println("  -----")
+		}
+		fmt.Println()
+
+		fmt.Println("  -------------------")
+		fmt.Println("  SmallImage")
+		fmt.Println("  -------------------")
+		printImage(&item.SmallImage)
+		fmt.Println("  -------------------")
+		fmt.Println("  MediumImage")
+		fmt.Println("  -------------------")
+		printImage(&item.MediumImage)
+		fmt.Println("  -------------------")
+		fmt.Println("  LargeImage")
+		fmt.Println("  -------------------")
+		printImage(&item.LargeImage)
+		fmt.Println()
+
+		itemAttributes := &item.ItemAttributes
+		fmt.Println("  -------------------")
+		fmt.Println("  ItemAttributes")
+		fmt.Println("  -------------------")
+		fmt.Printf("  EAN: %s\n", itemAttributes.EAN)
+		fmt.Printf("  EANs: %s\n", strings.Join(itemAttributes.EANs, ","))
+		fmt.Printf("  ISBN: %s\n", itemAttributes.ISBN)
+		fmt.Printf("  UPC: %s\n", itemAttributes.UPC)
+		fmt.Printf("  UPCs: %s\n", strings.Join(itemAttributes.UPCs, ","))
+		fmt.Printf("  Title: %s\n", itemAttributes.Title)
+		fmt.Printf("  Label: %s\n", itemAttributes.Label)
+		fmt.Printf("  Author: %s\n", itemAttributes.Author)
+		fmt.Printf("  Manufacturer: %s\n", itemAttributes.Manufacturer)
+		fmt.Printf("  Publisher: %s\n", itemAttributes.Publisher)
+		fmt.Printf("  Studio: %s\n", itemAttributes.Studio)
+		fmt.Printf("  Brand: %s\n", itemAttributes.Brand)
+		fmt.Printf("  ProductGroup: %s\n", itemAttributes.ProductGroup)
+		fmt.Printf("  ProductTypeName: %s\n", itemAttributes.ProductTypeName)
+		fmt.Printf("  Binding: %s\n", itemAttributes.Binding)
+		fmt.Printf("  Edition: %s\n", itemAttributes.Edition)
+		fmt.Printf("  PublicationDate: %s\n", itemAttributes.PublicationDate)
+		fmt.Printf("  Feature: %s\n", itemAttributes.Feature)
+
+		fmt.Println("    -------------------")
+		fmt.Println("    Languages")
+		fmt.Println("    -------------------")
+		for j, language := range itemAttributes.Languages {
+			fmt.Printf("    Language %d\n", j)
+			fmt.Printf("    Name: %s\n", language.Name)
+			fmt.Printf("    Type: %s\n", language.Type)
+			fmt.Println("    -----")
+		}
+
+		fmt.Println("    -------------------")
+		fmt.Println("    ItemDimensions")
+		fmt.Println("    -------------------")
+		fmt.Printf("    Height: %d\n", itemAttributes.ItemDimensions.Height)
+		fmt.Printf("    Width: %d\n", itemAttributes.ItemDimensions.Width)
+		fmt.Printf("    Length: %d\n", itemAttributes.ItemDimensions.Length)
+		fmt.Printf("    Weight: %d\n", itemAttributes.ItemDimensions.Weight)
+
+		fmt.Println("    -------------------")
+		fmt.Println("    PackageDimensions")
+		fmt.Println("    -------------------")
+		fmt.Printf("    Height: %d\n", itemAttributes.PackageDimensions.Height)
+		fmt.Printf("    Width: %d\n", itemAttributes.PackageDimensions.Width)
+		fmt.Printf("    Length: %d\n", itemAttributes.PackageDimensions.Length)
+		fmt.Printf("    Weight: %d\n", itemAttributes.PackageDimensions.Weight)
+		fmt.Println()
+
+		fmt.Println("  -------------------")
+		fmt.Println("  CustomerReviews")
+		fmt.Println("  -------------------")
+		fmt.Printf("  CustomerReviewsUrl: %s\n", item.CustomerReviewsURL)
+	}
+
+}
+
 func isFileExists(filename string) bool {
 	os.Mkdir("testfiles", 0777)
 	_, err := os.Stat(filename)
@@ -88,4 +198,10 @@ func checkAndWriteFile(t *testing.T, file string, response []byte, err error) {
 			t.Error(err)
 		}
 	}
+}
+
+func printImage(image *Image) {
+	fmt.Printf("  URL: %s\n", image.URL)
+	fmt.Printf("  Height: %d\n", image.Height)
+	fmt.Printf("  Width: %d\n", image.Width)
 }
--- xml.go
+++ xml.go
@@ -11,50 +11,18 @@ import (
 	"encoding/xml"
 )
 
-type Image struct {
-	URL    string
-	Height uint16
-	Width  uint16
-}
-
-type ItemLink struct {
-	Description string
-	URL         string
-}
-
-type Item struct {
-	XMLName       xml.Name `xml:"Item"`
-	ASIN          string
-	URL           string
-	DetailPageURL string
-	Title         string     `xml:"ItemAttributes>Title"`
-	Author        string     `xml:"ItemAttributes>Author"`
-	Price         string     `xml:"ItemAttributes>ListPrice>FormattedPrice"`
-	PriceRaw      string     `xml:"ItemAttributes>ListPrice>Amount"`
-	SmallImage    Image      `xml:"SmallImage"`
-	MediumImage   Image      `xml:"MediumImage"`
-	LargeImage    Image      `xml:"LargeImage"`
-	ItemLinks     []ItemLink `xml:"ItemLinks>ItemLink"`
-}
+//+-------------------------------------------------
+//| Request
+//+-------------------------------------------------
 
 type Request struct {
-	XMLName           xml.Name          `xml:"Request"`
-	IsValid           bool              `xml:"IsValid"`
-	ItemLookupRequest ItemLookupRequest `xml:"ItemLookupRequest"`
+	XMLName xml.Name `xml:"Request"`
+	IsValid bool     `xml:"IsValid"`
 }
 
-type ItemLookupRequest struct {
-	XMLName        xml.Name `xml:"ItemLookupRequest"`
-	IdType         string
-	ItemIds        []string `xml:"ItemId"`
-	ResponseGroups []string `xml:"ResponseGroup"`
-	VariationPage  string
-}
-
-type ItemResponseBase struct {
-	Items   []Item  `xml:"Items>Item"`
-	Request Request `xml:"Items>Request"`
-}
+//+-------------------------------------------------
+//| Response
+//+-------------------------------------------------
 
 // Mapping xml to struct.
 //
@@ -81,4 +49,78 @@ type ItemSearchResponse struct {
 	TotalResults         uint16   `xml:"Items>TotalResults"`
 	TotalPages           uint16   `xml:"Items>TotalPages"`
 	MoreSearchResultsUrl string   `xml:"Items>MoreSearchResultsUrl"`
+}
+
+type ItemResponseBase struct {
+	Items   []Item  `xml:"Items>Item"`
+	Request Request `xml:"Items>Request"`
+}
+
+//+-------------------------------------------------
+//| ResponseGroup
+//+-------------------------------------------------
+
+// ResponseGroup
+type ItemAttributes struct {
+	XMLName           xml.Name `xml:"ItemAttributes"`
+	EAN               string
+	EANs              []string `xml:"EANList>EANListElement"`
+	ISBN              string
+	UPC               string
+	UPCs              []string `xml:"UPCList>UPCListElement"`
+	Title             string
+	Label             string
+	Author            string
+	Manufacturer      string
+	Publisher         string
+	Studio            string
+	Brand             string
+	ProductGroup      string
+	ProductTypeName   string
+	Binding           string
+	Edition           string
+	PublicationDate   string
+	Feature           string
+	Languages         []Language `xml:"Languages>Language"`
+	ItemDimensions    Dimension  `xml:"ItemDimensions"`
+	PackageDimensions Dimension  `xml:"PackageDimensions"`
+}
+
+//+-------------------------------------------------
+//| Response Parts
+//+-------------------------------------------------
+
+type Dimension struct {
+	Height uint16
+	Width  uint16
+	Length uint16
+	Weight uint16
+}
+type Language struct {
+	Name string
+	Type string
+}
+
+type Image struct {
+	URL    string
+	Height uint16
+	Width  uint16
+}
+
+type ItemLink struct {
+	Description string
+	URL         string
+}
+
+type Item struct {
+	XMLName            xml.Name `xml:"Item"`
+	ASIN               string
+	URL                string
+	DetailPageURL      string
+	ItemAttributes     ItemAttributes `xml:"ItemAttributes"`
+	SmallImage         Image          `xml:"SmallImage"`
+	MediumImage        Image          `xml:"MediumImage"`
+	LargeImage         Image          `xml:"LargeImage"`
+	ItemLinks          []ItemLink     `xml:"ItemLinks>ItemLink"`
+	CustomerReviewsURL string         `xml:"CustomerReviews>IFrameURL"`
 }
